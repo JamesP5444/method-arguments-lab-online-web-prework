7e534437eba6e3c7d0146a44fe76ab03708ec9fa
github username <github email address>
2019-02-11 10:29:18 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction (name)
+  puts "Hi, my name is #{Josh}."
+end
+introduction
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}"
+end
+introduction_with_language
